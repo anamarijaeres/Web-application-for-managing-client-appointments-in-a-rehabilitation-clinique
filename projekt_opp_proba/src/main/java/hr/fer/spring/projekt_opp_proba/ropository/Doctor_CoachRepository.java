c56adc386256170610080b981401525f1b8b8ac6
JuricicJan <jan.juricic@fer.hr>
2019-12-19 18:48:52 +0100

    slucajno sam pushao projekt_opp_proba, logout i dalje ne radi

--- projekt_opp_proba/src/main/java/hr/fer/spring/projekt_opp_proba/ropository/Doctor_CoachRepository.java
+++
@@ -1,9 +0,0 @@
-package hr.fer.spring.projekt_opp_proba.ropository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import hr.fer.spring.projekt_opp_proba.model.Doctor_Coach;
-
-public interface Doctor_CoachRepository extends JpaRepository<Doctor_Coach, String>{
-
-}
--- projekt_opp_proba/src/main/java/hr/fer/spring/projekt_opp_proba/ropository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package hr.fer.spring.projekt_opp_proba.ropository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import hr.fer.spring.projekt_opp_proba.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-	
-}
